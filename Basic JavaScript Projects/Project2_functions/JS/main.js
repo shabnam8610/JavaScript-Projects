bf4b5d188877f2426f521ca99ae4d147b661a4fb
shabnam8610 <Shabnam.teimoori@gmail.com>
2026-09-17 10:50:11 -0400

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+// This function demonstrates the += operator.
+function myFunction() {
+
+    // Start with the first part of the sentence.
+    let sentence = "I am learning";
+
+    // The += operator adds more text to the existing string.
+    sentence += " JavaScript functions!";
+
+    // Display the completed sentence in the HTML element.
+    document.getElementById("concatenate").innerHTML = sentence;
+}
